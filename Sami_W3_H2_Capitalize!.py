efb4d7b9268a5bf8902c412de0eebc86334467df
Sami Arslan <143827564+SamiAslan@users.noreply.github.com>
2023-10-31 11:10:54 +0100

    Add files via upload

---
+++ Sami_W3_H2_Capitalize!.py
@@ -0,0 +1,15 @@
+#  https://www.hackerrank.com/challenges/capitalize/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+import string
+def solve(s):
+    #return " ".join(i.capitalize()  for i in s.split(" "))
+    return string/capwords(s, " ")
+if __name__ == '__main__':
